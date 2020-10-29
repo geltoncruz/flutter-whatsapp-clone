b0a6218f568667894559285d00101afc10646f9b
Gelton Cruz <gelton@midiaplural.com.br>
2020-10-29 19:19:30 -0400

    ajouté cam onglet et menu rolant

--- lib/widgets/appBarAndTabs.dart
+++ lib/widgets/appBarAndTabs.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp/pages/whatsAppCamera.dart';
 
 class AppBarAndTabs extends StatelessWidget {
   String title = "Whatsapp";
@@ -25,14 +26,30 @@ class AppBarAndTabs extends StatelessWidget {
                   print('Search tapped'),
                 },
               ),
-              IconButton(
-                icon: Icon(
-                  Icons.more_vert,
-                  color: Color(0XFFFFFFFF),
-                ),
-                onPressed: () => {
-                  print('More option tapped'),
-                },
+              PopupMenuButton(
+                icon: Icon(Icons.more_vert),
+                itemBuilder: (context) => [
+                  PopupMenuItem(
+                    child: Text("New group"),
+                    value: 0,
+                  ),
+                  PopupMenuItem(
+                    child: Text("New Difusion"),
+                    value: 1,
+                  ),
+                  PopupMenuItem(
+                    child: Text("WhatsApp Web"),
+                    value: 2,
+                  ),
+                  PopupMenuItem(
+                    child: Text('Importants messages'),
+                    value: 3,
+                  ),
+                  PopupMenuItem(
+                    child: Text('Settings'),
+                    value: 4,
+                  ),
+                ],
               )
             ],
             bottom: TabBar(
